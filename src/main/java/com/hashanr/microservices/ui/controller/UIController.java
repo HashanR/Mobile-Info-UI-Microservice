d7a5ae5ca305d62335b2210e9eed9bfa86e2e43b
HashanR <rmhashan@gmail.com>
2020-05-25 06:44:52 +0530

    Add coockie session and give http endpointwise security

--- src/main/java/com/hashanr/microservices/ui/controller/UIController.java
+++ src/main/java/com/hashanr/microservices/ui/controller/UIController.java
@@ -1,15 +1,40 @@
 package com.hashanr.microservices.ui.controller;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class UIController {
+@EnableOAuth2Sso
+public class UIController extends WebSecurityConfigurerAdapter {
+
 	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+		.antMatchers("/")
+		.permitAll()
+		.anyRequest()
+		.authenticated();
+	}
+
+
+
 	@RequestMapping(value="/")
 	public String loadUI()
 	{
 		return "home";
+	}
+	
+	
+	
+	@RequestMapping(value="/secure")
+	public String loadSecuredUI()
+	{
+		return "secure";
 	}
 
 }
